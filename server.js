c43b5737ded4fb731b2c4c069143152ae55c4295
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2022-09-16 14:57:15 +0300

    socket.io demo last touches and finished

--- server.js
+++ server.js
@@ -1,37 +1,32 @@
-const express = require("express")
-const http = require("http")
-const socketIO = require("socket.io")
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
 
-const app = express()
-const server = http.createServer(app)
-const io = socketIO(server)
-
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server); // exposes /socket.io endpoint!
 
 io.on("connection", (socket) => {
-    console.log("User Connected")
-    io.emit("message","hello")
+    console.log("user connected");
+    io.emit("chat message", "hello");
 
+    socket.on("private message", (message) => {
+        socket.emit("chat message", message);
+    });
 
-    socket.on("private message", message => {
-        socket.emit("chat message", message)
-    })
+    socket.on("global message", (message) => {
+        io.emit("chat message", message);
+    });
 
-    socket.on("global message", message => {
-        socket.emit("chat message", message)
-    })
+    socket.on("broadcast message", (message) => {
+        socket.broadcast.emit("chat message", message);
+    });
+});
 
-    socket.on("broadcast message", message => {
-        socket.emit("chat message", message)
-    })
-
-})
-
-
-app.get("/",(request,response) => {
-    response.sendFile(__dirname + "/index.html") // status code : OK
-})
-
+app.get("/", (request, response) => {
+    response.sendFile(__dirname + "/index.html");
+});
 
 server.listen(5000, () => {
-    console.log("Server is up and running on port 5000")
-})
+    console.log("server is up and running on port 5000");
+});
